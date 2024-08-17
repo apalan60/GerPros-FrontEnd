39e47a5faf5da1f2237c4fa360683557273a0510
Jan Lee <nn750137@gmail.com>
2024-08-18 01:31:59 +0800

    chore: Added Vuetify.

--- app-gerpros/app.vue
+++ app-gerpros/app.vue
@@ -1,15 +1,7 @@
-<script setup>
-const version = 2 + 1;
-</script>
-
 <template>
-  <div class="hello">Hello Nuxt {{ version }}!</div>
+  <NuxtLayout>
+    <v-app>
+      <NuxtPage />
+    </v-app>
+  </NuxtLayout>
 </template>
-
-<style scoped>
-.hello {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 3rem;
-  padding: 2rem;
-}
-</style>
---
+++ app-gerpros/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="hello">Hello Nuxt :D {{ version }}!</div>
+  </div>
+  <v-sheet class="mx-auto" width="300">
+    <v-form @submit.prevent>
+      <v-text-field
+        v-model="firstName"
+        :rules="rules"
+        label="First name"
+      ></v-text-field>
+      <v-btn class="mt-2" type="submit" block>Submit</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 2rem;
+}
+</style>
