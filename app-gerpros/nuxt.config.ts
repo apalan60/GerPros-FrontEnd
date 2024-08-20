134c92eb711fa303a78471f044b195804839894b
Jan Lee <nn750137@gmail.com>
2024-08-20 23:08:23 +0800

    chore: Removed Vuetify

--- app-gerpros/nuxt.config.ts
+++ app-gerpros/nuxt.config.ts
@@ -1,30 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
-
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  build: {
-    transpile: ['vuetify'],
-  },
-  modules: [
-    (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        // @ts-expect-error for vuetify import
-        config.plugins.push(vuetify({ autoImport: true }));
-      });
-    },
-    '@nuxt/eslint',
-  ],
+  build: {},
+  modules: ['@nuxt/eslint'],
   eslint: {
     checker: true,
-  },
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
-      },
-    },
   },
 });
