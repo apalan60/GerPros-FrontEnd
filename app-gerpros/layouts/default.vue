4619d7bcb0b4aa7309faff13f1c57fc499985ce7
Jan Lee <nn750137@gmail.com>
2024-08-22 07:58:09 +0800

    feat: Separated layout.

---
+++ app-gerpros/layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <UHeader :links="links">
+      <template #logo>
+        <Logo class="w-auto h-6" />
+      </template>
+      <UHorizontalNavigation
+        :links="links"
+        class="border-b border-gray-200 dark:border-gray-800 justify-center"
+      />
+      <UCommandPalette :empty-state="null" />
+    </UHeader>
+    <div class="container h-4/5">
+      <slot />
+    </div>
+    <ColorScheme
+      ><USelect
+        v-model="$colorMode.preference"
+        :options="['system', 'light', 'dark']"
+    /></ColorScheme>
+  </div>
+</template>
+
+<script setup>
+const links = [
+  { label: '產品項目', to: '/products' },
+  {
+    label: '產品細項 1(test)',
+    to: '/products/1',
+  },
+  { label: '主題專欄', to: '/topic' },
+  { label: '聯絡方式', to: '/contact' },
+  { label: '常見問題', to: '/FAQ' },
+  { label: '關於我們', to: '/aboutUs' },
+  { label: '服務項目', to: '/services' },
+];
+</script>
--- app-gerpros/pages/index.vue
+++ app-gerpros/pages/index.vue
@@ -1,35 +1,8 @@
 <template>
-  <UHeader :links="links">
-    <template #logo>
-      <Logo class="w-auto h-6" />
-    </template>
-    <UHorizontalNavigation
-      :links="links"
-      class="border-b border-gray-200 dark:border-gray-800 justify-center"
-    />
-    <UCommandPalette :empty-state="null" />
-    <ColorScheme
-      ><USelect
-        v-model="$colorMode.preference"
-        :options="['system', 'light', 'dark']"
-    /></ColorScheme>
-  </UHeader>
+  <div>home-page</div>
 </template>
 
-<script setup>
-const links = [
-  { label: '產品項目', to: '/products' },
-  {
-    label: '產品細項 1(test)',
-    to: '/products/1',
-  },
-  { label: '主題專欄', to: '/topic' },
-  { label: '聯絡方式', to: '/contact' },
-  { label: '常見問題', to: '/FAQ' },
-  { label: '關於我們', to: '/aboutUs' },
-  { label: '服務項目', to: '/services' },
-];
-</script>
+<script setup></script>
 
 <style scoped>
 .hello {
