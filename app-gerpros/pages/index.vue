34638884471f546d7cbf25fd7b69a7e0448b3900
apalan60 <apalan60@gmail.com>
2024-08-24 19:18:22 +0800

    feat: add carousel

--- app-gerpros/pages/index.vue
+++ app-gerpros/pages/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <div class="todo portfolio w-screen min-h-5 bg-slate-400">完工照</div>
+    <div class="todo portfolio w-screen= min-h-5 ">
+      <UContainer>
+      <UCarousel 
+      ref="carouselRef"
+      v-slot="{ item }" 
+      :items="items" 
+      :ui="{ item: 'basis-full' }" 
+      class=" rounded-lg overflow-hidden">
+    <img :src="item" class="w-full h-full" draggable="false">
+  </UCarousel>
+</UContainer>
+    </div>
     <div class="todo service w-screen min-h-5 bg-slate-200">
       服務的內容幫助過多少客戶
     </div>
@@ -14,6 +25,30 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const items = [
+  'https://picsum.photos/600/800?random=1',
+  'https://picsum.photos/600/800?random=2',
+  'https://picsum.photos/600/800?random=3',
+  'https://picsum.photos/600/800?random=4',
+  'https://picsum.photos/600/800?random=5',
+  'https://picsum.photos/600/800?random=6'
+]
+
+const carouselRef = ref()
+
+onMounted(() => {
+  setInterval(() => {
+    if (!carouselRef.value) return
+
+    if (carouselRef.value.page === carouselRef.value.pages) {
+      return carouselRef.value.select(0)
+    }
+
+    carouselRef.value.next()
+  }, 4000)
+})
+
+</script>
 
 <style scoped></style>
