8d34bff49a134f1cfe0a591eb55a9de4cc036927
apalan60 <apalan60@gmail.com>
2024-08-27 22:49:21 +0800

    chore: import nuxt/image

--- app-gerpros/nuxt.config.ts
+++ app-gerpros/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image'],
   compatibilityDate: '2024-08-22',
   eslint: {
     checker: true,
