1ff67ee27051e30995ff7d15c8286fb4b5838d8b
apalan60 <apalan60@gmail.com>
2024-09-07 17:25:45 +0800

    fix: toggle error with default data-theme

--- app-gerpros/plugins/themeChange.client.js
+++ app-gerpros/plugins/themeChange.client.js
@@ -1,6 +1,7 @@
 import { themeChange } from 'theme-change';
 
 export default defineNuxtPlugin(() => {
-  document.documentElement.setAttribute('data-theme', 'autumn');
-  themeChange(false);
+  onMounted(() => {
+    themeChange(false)
+  })
 });
