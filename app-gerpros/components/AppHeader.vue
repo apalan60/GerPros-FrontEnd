46cd1929d3383bcabd16da27b3d1ab84a788a12e
apalan60 <apalan60@gmail.com>
2024-09-07 16:50:38 +0800

    feat: add reponsive dropdown menu in navbar

--- app-gerpros/components/AppHeader.vue
+++ app-gerpros/components/AppHeader.vue
@@ -1,19 +1,50 @@
 <template>
   <div class="navbar bg-base-100">
     <div class="navbar-start">
-      <nuxt-link to="/" class="btn btn-ghost text-xl"> GerPros </nuxt-link>
+      <div class="dropdown lg:hidden">
+        <div tabindex="0" role="button" class="btn btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </div>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[50] mt-3 w-52 p-2 shadow"
+        >
+          <li v-for="link in links" :key="link.name">
+            <nuxt-link :to="link.to">{{ link.label }}</nuxt-link>
+          </li>
+        </ul>
+      </div>
+
+      <nuxt-link to="/" class="btn btn-ghost text-xl">GerPros</nuxt-link>
     </div>
+
     <div class="navbar-center hidden lg:flex">
-      <nuxt-link
-        v-for="link in links"
-        :key="link.name"
-        :to="link.to"
-        class="menu menu-horizontal px-1 hover:text-accent"
-        :class="{ 'text-sky-700': $route.path === link.to }"
-      >
-        {{ link.label }}
-      </nuxt-link>
+      <ul class="menu menu-horizontal px-1">
+        <li v-for="link in links" :key="link.name">
+          <nuxt-link
+            :to="link.to"
+            :class="{ 'text-sky-700': $route.path === link.to }"
+            class="hover:text-accent"
+          >
+            {{ link.label }}
+          </nuxt-link>
+        </li>
+      </ul>
     </div>
+
     <div class="navbar-end">
       <button class="btn btn-ghost btn-circle">
         <ThemeToggle />
