cc2afad6cd70312130ea58d6af2e045fbb369c40
apalan60 <apalan60@gmail.com>
2024-12-12 00:32:57 +0800

    Implement toast component and function 'useToast'

---
+++ app-gerpros/components/ToastMessages.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="toast toast-top toast-end" v-if="toastMessage">
+    <div
+        :class="[
+        'alert',
+        toastType === 'success' ? 'alert-success' : 'alert-error'
+      ]"
+    >
+      <span>{{ toastMessage }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useToast } from '~/composables/useToast';
+
+const { toastMessage, toastType } = useToast();
+</script>
