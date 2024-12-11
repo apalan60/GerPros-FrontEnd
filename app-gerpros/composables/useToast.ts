3f698adde1a372accf3a907a5c5eefdd0a806c61
apalan60 <apalan60@gmail.com>
2024-12-12 00:32:57 +0800

    Implement toast component and function 'useToast'

---
+++ app-gerpros/composables/useToast.ts
@@ -0,0 +1,24 @@
+import { ref } from 'vue';
+
+const toastMessage = ref('');
+const toastType = ref<'success' | 'error'>('success');
+
+let timer: ReturnType<typeof setTimeout> | null = null;
+
+export function useToast() {
+    const showToast = (type: 'success' | 'error', message: string, duration = 2000) => {
+        toastType.value = type;
+        toastMessage.value = message;
+
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(() => {
+            toastMessage.value = '';
+        }, duration);
+    };
+
+    return {
+        toastMessage,
+        toastType,
+        showToast
+    };
+}
