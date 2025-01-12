b29d83a734dcaa064959a59e63bbb0b172e1e0b2
Jiaen Lee <nn750137@gmail.com>
2025-01-12 18:48:32 +0800

    feat: to the top

--- app-gerpros/layouts/default.vue
+++ app-gerpros/layouts/default.vue
@@ -6,8 +6,21 @@
     </div>
     <AppFooter />
     <SecondAppFooter />
+    <img class="to-top cursor-pointer" width="64" height="64" src="/image/icons8-top-100.webp" alt="" @click="toTheTop">
   </div>
 </template>
+
+
+<script setup>
+function toTheTop() {
+  console.log('toTheTop');
+  
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+</script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100..900&display=swap');
@@ -17,5 +30,12 @@
   font-optical-sizing: auto;
   font-weight: 400;
   font-style: normal;
+}
+
+.to-top {
+  position: fixed;
+  right: 20px;
+  bottom: 20px;
+  z-index: 1000;
 }
 </style>
