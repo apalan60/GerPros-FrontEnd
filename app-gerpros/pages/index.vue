3951ac480ec81534464456be159bf37a5b27f232
Jiaen Lee <nn750137@gmail.com>
2025-01-13 02:13:01 +0800

    feat: main page cover image

--- app-gerpros/pages/index.vue
+++ app-gerpros/pages/index.vue
@@ -17,7 +17,7 @@ register();
         <swiper-slide class="slide slide3" />
       </swiper-container>
       <div class="title-text">
-        <img src="/image/logo.webp" alt="" width="500" class="full-content">
+        <img src="/image/logo.webp" alt="" width="500" class="full-content" >
       </div>
     </div>
     <div class="mb-20">
@@ -50,24 +50,42 @@ register();
 }
 .slide {
   background-position: top center;
-  background-size: cover;
   animation: zoom 7s infinite alternate;
 }
 .slide1 {
-  background: url('https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+  background: url('/image/topic-header-main.webp');
 }
 .slide2 {
-  background: url('https://plus.unsplash.com/premium_photo-1670360414946-e33a828d1d52?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+  background: url('/image/service-header-main.webp');
 }
 .slide3 {
-  background: url('https://images.unsplash.com/photo-1512972972907-6d71529c5e92?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+  background: url('/image/qa-header-main.webp');
+}
+
+@media (max-width: 900px) {
+  .slide {
+    background-size: cover;
+    animation: none;
+  }
+}
+
+@media (max-width: 798px) {
+  .slide1 {
+    background: url('/image/topic-header-mobile.webp');
+  }
+  .slide2 {
+    background: url('/image/service-header-mobile.webp');
+  }
+  .slide3 {
+    background: url('/image/qa-header-mobile.webp');
+  }
 }
 @keyframes zoom {
   0% {
-    background-size: 115%;
+    background-size: 105%;
   }
   100% {
-    background-size: 105%;
+    background-size: 100%;
   }
 }
 </style>
