260be7382e0fbd54362be283693d318f580a98a8
Jiaen Lee <nn750137@gmail.com>
2025-01-14 01:53:19 +0800

    feat: wip: testimonial

--- app-gerpros/components/mainPage/Testimonial.vue
+++ app-gerpros/components/mainPage/Testimonial.vue
@@ -1,120 +1,68 @@
 <template>
   <section>
     <div class="max-w-6xl mx-auto text-center">
-      <h2 class="text-xl font-bold">
-        客戶回饋
-      </h2>
-      <div class="grid sm:grid-cols-2 lg:grid-cols-1 gap-8 mt-12">
-        <div
-          v-for="testimonial in testimonials"
-          :key="testimonial.name"
-          class="bg-neutral text-neutral-content p-6 rounded-lg digit"
+      <h2 class="text-xl font-bold">客戶回饋</h2>
+      <div class="mt-10 h-[200px]">
+        <swiper-container
+          class="slide-container w-full h-full"
+          pagination="true"
+          slides-per-view="3"
+          space-between="30" 
+          loop="true"
+          autoplay-delay="3000"
         >
-          <p class="text-lg min-h-12 digit-test">
-            "{{ testimonial.quote }}"
-          </p>
-          <div class="mt-6 flex items-center">
-            <img
-              :src="testimonial.image"
-              alt="avatar"
-              class="w-12 h-12 rounded-full"
-            >
-            <div class="ml-4">
-              <h4 class="text-lg font-semibold">
-                {{ testimonial.name }}
-              </h4>
-              <p class="text-sm">
-                {{ testimonial.position }}
-              </p>
+          <swiper-slide v-for="test in testimonials" :key="test.name" class="slide flex items-center" >
+            <div class="bg-gray-200 flex items-center justify-center shadow-lg">
+              <img :src="test.image" alt="avatar" class="w-20 h-20 rounded-full m-4">
+              <div class="w-[300px] text-left my-4 me-4">
+                <p class="text-sm mb-1">{{ test.position }} {{ test.name }}</p>
+                <p class="text-sm">{{ test.quote }}</p>
+              </div>
             </div>
-          </div>
-        </div>
+          </swiper-slide>
+        </swiper-container>
       </div>
     </div>
   </section>
 </template>
 
 <script setup>
-onMounted(async () => {
-  if (typeof window !== 'undefined') {
-    const ScrollMagic = (await import('scrollmagic')).default;
+import { register } from 'swiper/element/bundle';
+import 'swiper/swiper-bundle.css';
 
-    const controller = new ScrollMagic.Controller();
-
-    const revealElements = document.querySelectorAll('.digit');
-
-    revealElements.forEach((element) => {
-      new ScrollMagic.Scene({
-        triggerElement: element,
-        triggerHook: 0.9,
-        reverse: true,
-      })
-        .setClassToggle(element, 'visible')
-        .addTo(controller);
-    });
-  }
-});
+register();
 
 const testimonials = ref([
   {
-    name: '王先生',
-    position: '台南市',
+    name: '李先生',
+    position: '高雄市',
     image:
       'https://cdn.pixabay.com/photo/2017/02/23/13/05/avatar-2092113_1280.png',
-    quote: '非常好的一次消費體驗',
-  },
-  {
-    name: '徐先生',
-    position: '台北市',
-    image: 'https://www.w3schools.com/howto/img_avatar.png',
-    quote: '品質非常良好',
-  },
-  {
-    name: '劉小姐',
-    position: '台中市',
-    image:
-      'https://th.bing.com/th/id/OIP.8F97-4tTe0kDw2M0XktW8gHaHZ?rs=1&pid=ImgDetMain',
-    quote: '家人都很喜歡',
+    quote: '邱業務 講解詳細 談話風趣 能幫客戶找到屬於心儀 的地板樣式，會再回購',
   },
   {
     name: '陳小姐',
-    position: '嘉義市',
+    position: '台南市',
     image:
-      'https://th.bing.com/th/id/R.5522155776f935b968ee033b378c7dce?rik=oHI71v3KwMtjtA&riu=http%3a%2f%2fwww.w3schools.com%2fhowto%2fimg_avatar2.png&ehk=v5Am%2bIj3lmC1HosplNbkqiz5jntcbPnbQ%2bvttJpmDHA%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
-    quote: '是個好選擇，推薦給有需要的人',
+    'https://th.bing.com/th/id/OIP.8F97-4tTe0kDw2M0XktW8gHaHZ?rs=1&pid=ImgDetMain',
+    quote: '價格公正實惠，師傅做工細心，非常推薦的店家',
+  },
+  {
+    name: '李先生',
+    position: '台南市',
+    image: 'https://www.w3schools.com/howto/img_avatar.png',
+    quote: '設計師指定選用Art Floor超耐磨木地板，感謝公司 讓我們到現場看大板，也有部分色號有鋪面展示， 木地板很漂亮！',
+  },
+  {
+    name: '劉小姐',
+    position: '台南市',
+    image:
+      'https://th.bing.com/th/id/OIP.8F97-4tTe0kDw2M0XktW8gHaHZ?rs=1&pid=ImgDetMain',
+    quote: '做地板找小胖 服務態度好到沒話說',
   },
 ]);
 </script>
 
 <style scoped>
-.digit-test {
-  display: flex;
-  align-content: center;
-  align-items: center;
-}
 
-.digit {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  opacity: 0;
-  -webkit-transform: translateX(-40px);
-  -moz-transform: translateX(-40px);
-  -ms-transform: translateX(-40px);
-  -o-transform: translateX(-40px);
-  transform: translateX(-40px);
-  -webkit-transition: all 0.6s ease-out;
-  -moz-transition: all 0.6s ease-out;
-  -ms-transition: all 0.6s ease-out;
-  -o-transition: all 0.6s ease-out;
-  transition: all 0.6s ease-out;
-}
-.digit.visible {
-  opacity: 1;
-  -webkit-transform: none;
-  -moz-transform: none;
-  -ms-transform: none;
-  -o-transform: none;
-  transform: none;
-}
 </style>
