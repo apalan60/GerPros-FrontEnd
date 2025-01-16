82a589ed7242dcca302cd92b7eabf3ced48adef6
apalan60 <apalan60@gmail.com>
2025-01-16 23:57:27 +0800

    fix: handle situation of long description in posts

--- app-gerpros/components/topic/card.vue
+++ app-gerpros/components/topic/card.vue
@@ -2,38 +2,27 @@
   <div class="card lg:card-side lg:w-[100%] bg-base-300 shadow-xl mt-4">
     <figure>
       <img
-        class="lg:w-[225px] lg:min-h-[225px] lg:h-full h-[300px] w-[100%] object-cover object-center"
-        :src="props.topic.coverImage"
-        :alt="props.topic.title"
+          class="lg:w-[225px] lg:min-h-[225px] lg:h-full h-[300px] w-[100%] object-cover object-center"
+          :src="props.topic.coverImage"
+          :alt="props.topic.title"
       >
     </figure>
-    <div class="card-body w-[80%] bg-[#d2c2af]"> //todo 換成統一用的css? 改到一半無法了
+    <div class="card-body w-[100%] bg-[#d2c2af]">
       <h2 class="card-title">
         {{ props.topic.title }}
       </h2>
-      <p class="text-wrap">{{ props.topic.description }}</p>
-      <div class="card-actions justify-between items-center">
-        <div class="tags flex flex-wrap gap-2 ">
-          <div
-            v-for="tag in props.topic.tags"
-            :key="tag"
-            class="badge badge-outline cursor-pointer"
-            @click="clickTag(tag)"
-          >
-            {{ tag }}
-          </div>
-        </div>
-        <NuxtLink :to="{ name: 'topic-id', params: { id: props.topic?.id } }">
-          <button class="btn btn-primary">閱讀更多</button>
-        </NuxtLink>
-        <div v-if="isManager" class="flex gap-2">
-          <NuxtLink
-            
-            :to="{ name: 'manager-topic-id', params: { id: props.topic?.id } }"
-          >
-            <button class="btn btn-success">編輯</button>
+      <p class="text-wrap w-full max-w-sm break-words ">{{ props.topic.description }}</p>
+      <div class="card-actions justify-end items-center">
+        <div class="flex items-center gap-10">
+          <NuxtLink :to="{ name: 'topic-id', params: { id: props.topic?.id } }">
+            <button class="btn btn-primary">閱讀更多</button>
           </NuxtLink>
-          <button class="btn btn-error" @click="onClickDelete">刪除</button>
+          <div v-if="isManager" class="flex gap-2">
+            <NuxtLink :to="{ name: 'manager-topic-id', params: { id: props.topic?.id } }">
+              <button class="btn btn-success">編輯</button>
+            </NuxtLink>
+            <button class="btn btn-error" @click="onClickDelete">刪除</button>
+          </div>
         </div>
       </div>
     </div>
