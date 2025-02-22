b5f9920b96de0cc77cbf9571f58ec9473ae388e5
apalan60 <apalan60@gmail.com>
2025-02-22 17:44:04 +0800

    add random products to app footer

--- app-gerpros/components/AppFooter.vue
+++ app-gerpros/components/AppFooter.vue
@@ -109,12 +109,13 @@
       </h6>
       <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
         <div
-            v-for="(image, index) in images"
+            v-for="(product, index) in randomProducts"
             :key="index"
             class="overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300"
+            @click="goToProduct(product.id)"
         >
           <img
-              :src="image"
+              :src="product.image"
               loading="lazy"
               alt="Pet image"
               class="w-full h-16 object-cover"
@@ -128,12 +129,25 @@
 </template>
 
 <script setup>
-const images = [
-  'https://images.unsplash.com/photo-1522444195799-478538b28823?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  'https://images.unsplash.com/photo-1501876725168-00c445821c9e?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  'https://images.unsplash.com/photo-1527694224012-be005121c774?q=80&w=2576&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  'https://images.unsplash.com/photo-1523755231516-e43fd2e8dca5?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  'https://plus.unsplash.com/premium_photo-1678402545080-2353b489c0c3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  'https://images.unsplash.com/photo-1499955085172-a104c9463ece?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-];
+
+const randomProducts = ref([]);
+
+async function fetchRandomProducts() {
+  try {
+    const data = await useApiFetch('/ProductItems?PageNumber=1&PageSize=12');
+    if (data) {
+      randomProducts.value = data.items.sort(() => 0.5 - Math.random()).slice(0, 6);
+    }
+  } catch (error) {
+    console.error('Unable to fetch product data', error);
+  }
+}
+
+function goToProduct(id) {
+  navigateTo(`/products/${id}`);
+}
+
+onMounted(() => {
+  fetchRandomProducts();
+});
 </script>
