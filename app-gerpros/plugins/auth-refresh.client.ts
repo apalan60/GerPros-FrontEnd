0d6a4e22d1fd1e05a4dcc23c8af07c134a008092
apalan60 <apalan60@gmail.com>
2025-03-01 18:55:19 +0800

    fix: error of refresh token that using reference type to get auth status

--- app-gerpros/plugins/auth-refresh.client.ts
+++ app-gerpros/plugins/auth-refresh.client.ts
@@ -2,13 +2,20 @@
 import { useAuthStore } from '~/stores/auth';
 import { refreshTokens } from '~/composables/useApiFetch'; // 或者放在 store actions
 
+/**
+ * Nuxt plugin to periodically check and refresh authentication tokens.
+ */
 export default defineNuxtPlugin(() => {
   const auth = useAuthStore();
 
   const intervalTime = 180 * 1000; // 180 秒
   setInterval(async () => {
-    if (auth.isLoggedIn && auth.shouldRefresh) {
+    console.log('----checking tokens----');
+    console.log("login status:" +  auth.isLoggedIn.value, "/" +  "should refresh: " + auth.shouldRefresh.value);
+    if (auth.isLoggedIn.value && auth.shouldRefresh.value) {
+      console.log('refreshing tokens in progress');
       await refreshTokens();
+      console.log('----tokens refreshed----');
     }
   }, intervalTime);
 });
